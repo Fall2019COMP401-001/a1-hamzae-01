589382b744f954e3bb238083437be26712a9bb02
hamza <hamza@10.20.79.23>
2019-08-30 18:36:24 -0400

    adept pt1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,26 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int type = scan.nextInt();
+		// number of groceries
+		String [] typeName = new String [type];
+		double [] cost = new double [type];
+		for(int i=0; i<type; i++) {
+			typeName[i] = scan.next();
+			cost[i] = scan.nextDouble ();
 		
+		}
+		int count = scan.nextInt();
+		String[] firstName = new String[count];
+		String[] lastName = new String [count];
+		for(int j =0 ; j<count; j++) {
+			firstName[j] = scan.next();
+			lastName [j] = scan.next();
+			int groc = scan.nextInt();
+			for(int k=0; k<groc; k++) {
+				int quantity = scan.nextInt();
+				String typeName = scan.next();
+			}
 	}
+}
 }
