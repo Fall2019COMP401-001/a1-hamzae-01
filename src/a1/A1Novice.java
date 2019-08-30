031a47179fb3e49cfe834d79f7ea2635d5d611c0
hamza <hamza@10.20.79.23>
2019-08-30 00:38:55 -0400

    novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,30 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
+		// number of names^
+		String[] firstName = new String[count];
+		String[] lastName = new String [count];
+		// inserts names^
+		double[] totalCost = new double[count];
+		// adds all costs together^
+		for(int i=0; i<count; i++) {
+			firstName [i] = scan.next();
+			lastName [i] = scan.next();
+			int types = scan.nextInt();
+			for(int j=0; j<types; j++) {
+				// quantity of groceries
+			int quantity = scan.nextInt();
+			String typeName = scan.next();
+			double cost = scan.nextDouble ();
+			totalCost[i] += quantity * cost;
+			}
+		}
+		//end of forloop
+		for(int i = 0; i<count; i++) {
+			System.out.println(firstName[i].charAt(0) + ". " + lastName[i] + ": " + String.format("%.2f", totalCost[i]));
+			// prints all terms, "%.2f" adds the 0 to 12.5
+		}
 		
-	}
+}
 }
