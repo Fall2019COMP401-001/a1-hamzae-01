2dff79ff699953cc002cb18d54059e9cc0a68381
hamza <hamza@localhost>
2019-08-31 18:06:53 -0400

    adept final

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,37 +10,46 @@ public class A1Adept {
 
 		// Your code follows here.
 		int type = scan.nextInt();
-		// number of groceries
-		String [] typeName = new String [type];
+		// type is the number of groceries
+		String [] typeName = new String [type]; 
+		//typeName is the names of groceries stored for reference point later
 		double [] cost = new double [type];
+		// double cost represents price of each grocery
 		for(int i=0; i<type; i++) {
 			typeName[i] = scan.next();
 			cost[i] = scan.nextDouble ();
-		
 		}
+		//this for loop interprets the list of groceries depending on number inputed for type
 		int count = scan.nextInt();
+		//count is the amount of people
 		String[] firstName = new String[count];
 		String[] lastName = new String [count];
+		//arrays storing first and last names
 		Double[] totalCost = new Double [count];
-		for(int zero = 0; zero < count; zero++) {
-			totalCost[zero] = 0.0;
+		//array storing the final cost of each customer so that they can be compares for size and averaged out
+		for(int na = 0; na < count; na++) {
+			totalCost[na] = 0.0;
 		}
+		//below for loop goes through the names and has a nested for loop which goes through the list of groceries each buyer purchased
 		for(int a =0 ; a<count; a++) {			
 			firstName[a] = scan.next();
 			lastName [a] = scan.next();
 			int groc = scan.nextInt();
-			
+			//groc represents the number of different types of groceries each customer bought
 			for(int k=0; k<groc; k++) {
 				int quantity = scan.nextInt();
+				// quantity is the amount of each type of groc bought
 				String typenames = scan.next();
-				
+				//typenames represents the names of the groceries in the second section of the input
 			int index=0;
 				for(int l=0; l<type; l++) {
 					if(typenames.equals(typeName[l])) {
 						index = l;
 					}
+					//the above for loop references typenames back to typeName to get the price of each groc item
 				}
 				totalCost[a] += cost[index] * quantity;
+				//gets totalcost from above for loop
 			}
 		}
 		int big=0;
@@ -52,9 +61,10 @@ public class A1Adept {
 			}
 			if(totalCost[x] < totalCost[small]) {
 				small = x;
-				 
+			//this for loop compares the totalcost of each buyer and gets the biggest and smallest	 
 			}
 			sum += totalCost[x];
+			//sum of all 3 buyers uses in print statement to find average
 			//avg = (float)((totalCost[big]+totalCost[small])/2);
 		}
 		System.out.println("Biggest: " + firstName[big] + " " + lastName[big] + " (" + String.format("%.2f", totalCost[big]) + ")");
